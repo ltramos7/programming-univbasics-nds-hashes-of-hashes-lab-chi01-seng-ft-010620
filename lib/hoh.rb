17c0c342dad7902e4aee7728130f58a4fd08f8c9
Linda Ramos <ltramos7@gmail.com>
2019-12-23 17:41:04 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -49,7 +49,10 @@ def naming_system
       sub_category: {
         label: "Class",
         sub_category: {
-          label: "Order"
+          label: "Order",
+          sub_category: {
+            label: "Genus"
+          }
         }
       }
     }
